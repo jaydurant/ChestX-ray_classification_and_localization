533ff2549c7057b71cba937a8e39de6644952598
Jason Durant <durantjay1@gmail.com>
2021-12-12 13:29:39 -0500

    Fix typo

--- models/resnet50.py
+++ models/resnet50.py
@@ -6,12 +6,12 @@ class Resnet50(nn.Module):
     def __init__(self, n_classes):
         super(Resnet50, self).__init__()
         resnet = models.resnet50(pretrained=True)
-        resnet.avgpool = nn.Identity(2048)
+        resnet.avgpool = nn.Identity()
         resnet.fc= nn.Sequential(
-            nn.Conv2d(2048,2048, 1),
-            nn.ReLU(),
-            nn.AdaptiveAvgPool2d(output_size=(1,1)),
-            nn.Linear(in_features=2048,out_features=n_classes, bias=True)
+            #nn.Conv2d(2048,2048, 1),
+            #nn.ReLU(),
+            #nn.AdaptiveAvgPool2d(output_size=(1,1)),
+            #nn.Linear(in_features=2048,out_features=n_classes, bias=True)
         )
         self.base_model = resnet
 
